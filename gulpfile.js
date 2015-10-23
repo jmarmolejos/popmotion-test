13ce50f69d61c03267c49df4d9e8347663f7c710
Jose Marmolejos <marmolejos.jose@gmail.com>
2015-10-23 16:45:43 -0400

    Reorganize files into a saner structure.

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var browserify = require('browserify')
 
 gulp.task('sass', function () {
 // gulp.src('./sass/**/*.scss')
-  gulp.src('./sass/main.scss')
+  gulp.src('./src/sass/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('browserify', function() {
